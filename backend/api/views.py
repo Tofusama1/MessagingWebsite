2f4827d2a280673542d61a54d0060bebf4d712b0
tofusama1 <christan8@yahoo.com>
2025-01-03 14:10:57 -0500

    We have authenicate the users and now I am trying to create the model

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,5 +7,5 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all() 
     #this is a list of already created users so that we don't have to create a new use rif the user exist
-    serializer_class = User
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
--- backend/backend/api/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import JsonResponse
-
-def api_root(request):
-    return JsonResponse({"message": "Welcome to the API!"})
